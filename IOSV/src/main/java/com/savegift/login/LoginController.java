095e2693159a8d34a461c57116d071341f8e3ce4
SeungUkShin <krdut1@gmail.com>
2022-04-15 15:08:07 +0900

    [신승욱] Git Test2

--- IOSV/src/main/java/com/savegift/login/LoginController.java
+++ IOSV/src/main/java/com/savegift/login/LoginController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page. 
- * Git Commit Test
+ * Git Commit Testd
  */
 @Controller
 public class LoginController {
