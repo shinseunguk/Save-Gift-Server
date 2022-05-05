b9333002561a61acc458d027c1e937c386963ec0
SeungUkShin <krdut1@gmail.com>
2022-05-05 21:38:59 +0900

    [신승] 알림설정 작업

--- IOSV/src/main/java/com/savegift/notification/NotificationDAO.java
+++ IOSV/src/main/java/com/savegift/notification/NotificationDAO.java
@@ -31,19 +31,46 @@ public class NotificationDAO {
 		return loginVO;
 	}
 	
+	public NotificationVO status2(String device_id) {
+		NotificationVO notificationVO = mybatis.selectOne("NotificationMapper.status2", device_id);
+		if(notificationVO != null) {
+			logger.info("DAO status... " + notificationVO.getPush1());
+//			logger.info("DAO status... " + notificationVO.getPush_token());
+		}
+		
+		return notificationVO;
+	}
+	
 	public boolean notiSetting(HashMap<String, Object> requestMap) {
 		boolean resultBool = false;
 		
-		String user_id = (String) requestMap.get("user_id"); // 아이디
-		int push_yn = (Integer) requestMap.get("push_yn"); // 푸시 
-		int email_yn = (Integer) requestMap.get("email_yn"); // 이메일 
-		int sms_yn = (Integer) requestMap.get("sms_yn"); // 문자
+		int index = (Integer) requestMap.get("index"); // 문자
 		
-		logger.info("/notiSetting.. "+ user_id + " push_yn : " + push_yn + ", email_yn : " + email_yn + ", sms_yn : " + sms_yn);
-		
-		int result = mybatis.update("NotificationMapper.notiSetting", requestMap);
-		if(result == 1) {
-			resultBool = true;
+		if(index ==0) { // user_device UPDATE --> 푸시 푸시30 푸시7 푸시1
+			String device_id = (String) requestMap.get("device_id"); // 아이디
+			int push_yn = (Integer) requestMap.get("push_yn"); // 푸시	
+			int push30 = (Integer) requestMap.get("push30"); // 푸시	
+			int push7 = (Integer) requestMap.get("push7"); // 푸시	
+			int push1 = (Integer) requestMap.get("push1"); // 푸시
+			
+			logger.info("/notiSetting1.. "+ device_id + ", push_yn : " + push_yn + ", push30 : " + push30 + ", push7 : " + push7 + ", push1 : " + push1);
+			
+			int result = mybatis.update("NotificationMapper.notiSetting1", requestMap);
+			if(result == 1) {
+				resultBool = true;
+			}
+			
+		}else { // user 문자 이메일
+			String user_id = (String) requestMap.get("user_id"); // 아이디
+			int email_yn = (Integer) requestMap.get("email_yn"); // 이메일 
+			int sms_yn = (Integer) requestMap.get("sms_yn"); // 문자
+			
+			logger.info("/notiSetting2.. "+ user_id + ", email_yn : " + email_yn + ", sms_yn : " + sms_yn);
+			
+			int result = mybatis.update("NotificationMapper.notiSetting2", requestMap);
+			if(result == 1) {
+				resultBool = true;
+			}
 		}
 		
 		return resultBool;
