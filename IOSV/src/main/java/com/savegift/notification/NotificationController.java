9b5f459a580c27fac07952f57683e8b29c7a2c59
shinseunguk <krdut1@gmail.com>
2022-07-21 12:31:58 +0900

    refactor: context.xml 및 FCM keyValue 값 properties에서 불러오기

--- IOSV/src/main/java/com/savegift/notification/NotificationController.java
+++ IOSV/src/main/java/com/savegift/notification/NotificationController.java
@@ -8,6 +8,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.Locale;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +30,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -119,86 +121,144 @@ public class NotificationController {
 		
 		return true;
 	}
-	
+
 	@RequestMapping(value="/fcmTest", method=RequestMethod.GET, produces="text/plain;charset=UTF-8")
 	@ResponseBody
-	public boolean pushSend(HttpServletRequest request) throws Exception {
-		return notificationService.sendPush();
+	public void fcmTest(){
+		notificationService.sendPush();
 	}
 	
-	@RequestMapping(value = "/push/list", method = RequestMethod.GET)
-	@ResponseBody
-	public List<GiftUserDeviceVO> getPushList(){
-		Date date = new Date();
-		List<GiftUserDeviceVO> list = new ArrayList<GiftUserDeviceVO>();
-		List<GiftUserDeviceVO> list30 = new ArrayList<GiftUserDeviceVO>();
-		List<GiftUserDeviceVO> list7 = new ArrayList<GiftUserDeviceVO>();
-		List<GiftUserDeviceVO> list1 = new ArrayList<GiftUserDeviceVO>();
-		
-		Date[] arr30 = null;
-		Date[] arr7 = null;
-		Date[] arr1 = null;
-		
-		list = giftService.getPushList(date);
-		
-		for(int i = 0 ; i < list.size() ; i++) {
-			if(list.get(i).getPush30() == 1) {
-				list30.add(list.get(i));
-			}else if (list.get(i).getPush7() == 1) {
-				list7.add(list.get(i));
-			}else {
-				list1.add(list.get(i));
-			}
-		}
-		
-		System.out.println("!!!!");
-		for(int i = 0 ; i < list30.size(); i++) {
-			System.out.print(list30.get(i).getSeq()+" ");
-		}
-		System.out.println("@@@@@");
-		for(int i = 0 ; i < list7.size(); i++) {
-			System.out.print(list7.get(i).getSeq()+" ");
-		}
-		System.out.println("########");
-		for(int i = 0 ; i < list1.size(); i++) {
-			System.out.print(list1.get(i).getSeq()+" ");
-		}
-		
-		if(list30.size() != 0) {
-			arr30 = new Date[list30.size()];
-			for(int i = 0 ; i < list30.size(); i++) {
-				arr30[i] = list30.get(i).getExpiration_period();
-			}
-			System.out.println("");
-			for(int i = 0 ; i < arr30.length ; i++) {
-				System.out.print(arr30[i]+" ");
-			}
-		}
-		
-		if(list7.size() != 0) {
-			arr7 = new Date[list7.size()];
-			for(int i = 0 ; i < list7.size(); i++) {
-				arr7[i] = list7.get(i).getExpiration_period();
-			}
-			System.out.println("");
-			for(int i = 0 ; i < arr7.length ; i++) {
-				System.out.print(arr7[i]+" ");
-			}
-		}
-		
-		if(list1.size() != 0) {
-			arr1 = new Date[list1.size()];
-			for(int i = 0 ; i < list1.size(); i++) {
-				arr1[i] = list1.get(i).getExpiration_period();
-			}
-			System.out.println("");
-			for(int i = 0 ; i < arr1.length ; i++) {
-				System.out.print(arr1[i]+" ");
-			}
-		}
-		
-		return list;
+//	@RequestMapping(value="/fcmTest", method=RequestMethod.GET, produces="text/plain;charset=UTF-8")
+//	@ResponseBody
+	@Scheduled(cron = "0 0 9 * * *")
+	public void pushSend(){
+		notificationService.sendPush();
 	}
+	
+//	@RequestMapping(value = "/push/list", method = RequestMethod.GET)
+//	@ResponseBody
+//	public List<GiftUserDeviceVO> getPushList(){
+//		Date date = new Date();
+//		List<GiftUserDeviceVO> list = new ArrayList<GiftUserDeviceVO>();
+//		List<GiftUserDeviceVO> list30 = new ArrayList<GiftUserDeviceVO>();
+//		List<GiftUserDeviceVO> list7 = new ArrayList<GiftUserDeviceVO>();
+//		List<GiftUserDeviceVO> list1 = new ArrayList<GiftUserDeviceVO>();
+//		List<GiftUserDeviceVO> resultList = new ArrayList<GiftUserDeviceVO>();
+//		
+//		Date[] arr30 = null;
+//		Date[] arr7 = null;
+//		Date[] arr1 = null;
+//		
+//	      Calendar cal = Calendar.getInstance();
+//	      Calendar cal2 = Calendar.getInstance();
+//	      Calendar cal3 = Calendar.getInstance();
+//	      
+//	      cal.setTime(new Date());
+//	      cal2.setTime(new Date());
+//	      cal3.setTime(new Date());
+//	      DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//	      System.out.println("current: " + df.format(cal.getTime()));
+//	
+//	      cal.add(Calendar.MONTH, 1);
+//	      cal2.add(Calendar.DATE, 7);
+//	      cal3.add(Calendar.DATE, 1);
+//	      
+//	      Date mDate1 = cal.getTime();
+//	      Date mDate2 = cal2.getTime();
+//	      Date mDate3 = cal3.getTime();
+//	      
+//	      System.out.println("after: " + df.format(cal.getTime()));
+//	      System.out.println("after: " + df.format(cal2.getTime()));
+//	      System.out.println("after: " + df.format(cal3.getTime()));
+//		
+//		list = giftService.getPushList(date);
+//		
+//		for(int i = 0 ; i < list.size() ; i++) {
+//			if(list.get(i).getPush30() == 1) {
+//				list30.add(list.get(i));
+//			}else if (list.get(i).getPush7() == 1) {
+//				list7.add(list.get(i));
+//			}else {
+//				list1.add(list.get(i));
+//			}
+//		}
+//		
+////		System.out.println("!!!!");
+////		for(int i = 0 ; i < list30.size(); i++) {
+////			System.out.print(list30.get(i).getSeq()+" ");
+////		}
+////		System.out.println("@@@@@");
+////		for(int i = 0 ; i < list7.size(); i++) {
+////			System.out.print(list7.get(i).getSeq()+" ");
+////		}
+////		System.out.println("########");
+////		for(int i = 0 ; i < list1.size(); i++) {
+////			System.out.print(list1.get(i).getSeq()+" ");
+////		}
+//		
+//		//https://www.delftstack.com/ko/howto/java/how-to-compare-two-dates-in-java/#java%25EC%2597%2590%25EC%2584%259C-%25EB%2591%2590-%25EB%2582%25A0%25EC%25A7%259C%25EB%25A5%25BC-%25EB%25B9%2584%25EA%25B5%2590%25ED%2595%2598%25EB%258A%2594before%25EB%25A9%2594%25EC%2586%258C%25EB%2593%259C
+//		//date.before(date) 함수 이용해서 list 뽑아내기
+//		if(list30.size() != 0) {
+//			arr30 = new Date[list30.size()];
+//			for(int i = 0 ; i < list30.size(); i++) {
+//				arr30[i] = list30.get(i).getExpiration_period();
+//				if(arr30[i].equals(mDate1)) {
+////					logger.info("eqaul1 "+ list30.get(i).getExpiration_period());
+//					resultList.add(list30.get(i));
+//				}else if(arr30[i].before(mDate1)) {
+////					logger.info("before1 "+ list30.get(i).getExpiration_period());
+//					resultList.add(list30.get(i));
+//				}
+//			}
+////			System.out.println("");
+////			for(int i = 0 ; i < arr30.length ; i++) {
+////				System.out.print(arr30[i]+" ");
+////			}
+//		}
+//		
+//		if(list7.size() != 0) {
+//			arr7 = new Date[list7.size()];
+//			for(int i = 0 ; i < list7.size(); i++) {
+//				arr7[i] = list7.get(i).getExpiration_period();
+//				if(arr7[i].equals(mDate2)) {
+////					logger.info("eqaul2 "+ list7.get(i).getExpiration_period());
+//					resultList.add(list7.get(i));
+//				}else if(arr7[i].before(mDate2)) {
+////					logger.info("before2 "+ list7.get(i).getExpiration_period());
+//					resultList.add(list7.get(i));
+//				}
+//			}
+////			System.out.println("");
+////			for(int i = 0 ; i < arr7.length ; i++) {
+////				System.out.print(arr7[i]+" ");
+////			}
+//		}
+//		
+//		if(list1.size() != 0) {
+//			arr1 = new Date[list1.size()];
+//			for(int i = 0 ; i < list1.size(); i++) {
+//				arr1[i] = list1.get(i).getExpiration_period();
+//				if(arr1[i].equals(mDate3)) {
+////					logger.info("eqaul3 "+ list1.get(i).getExpiration_period());
+//					resultList.add(list1.get(i));
+//				}else if(arr1[i].before(mDate3)) {
+////					logger.info("before3 "+ list1.get(i).getExpiration_period());
+//					resultList.add(list1.get(i));
+//				}
+//			}
+////			System.out.println("");
+////			for(int i = 0 ; i < arr1.length ; i++) {
+////				System.out.print(arr1[i]+" ");
+////			}
+//		}
+//		
+//		System.out.println("");
+//		for(int i = 0 ; i < resultList.size() ; i++) {
+//			System.out.print(resultList.get(i).getSeq()+"\n");
+//		}
+//		
+//		return resultList;
+//	}
 	
 
 }
--- IOSV/src/main/java/com/savegift/notification/NotificationDAO.java
+++ IOSV/src/main/java/com/savegift/notification/NotificationDAO.java
@@ -5,6 +5,11 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,11 +19,19 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Repository;
 
+import com.savegift.giftcon.GiftDAO;
+import com.savegift.giftcon.GiftUserDeviceVO;
 import com.savegift.login.LoginVO;
 
 @Repository
+@PropertySource("classpath:datasource.properties")
 public class NotificationDAO {
 	
 	private static final Logger logger = LoggerFactory.getLogger(NotificationDAO.class);
@@ -26,105 +39,229 @@ public class NotificationDAO {
 	@Autowired
     SqlSession mybatis;
 	
+	@Autowired
+	GiftDAO giftDAO;
+	
+	@Value("${fcm.keyValue}")
+	private String authKey;
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
 	public boolean sendPush() {
-		String[] registerIdsArr = new String[1000]; 
-		//FCM apikey authkey 은닉
-		String authKey = "AAAAmjgVFQY:APA91bHnP115Fl_CR0RcJPpW4l2x7lnluAhlAzr-_c-eFySEwuA2-jYIEFdQThmgReRGP9P-4Ek-0mysGT1NEcQgK5T9bGuQcMn3qqskvLgVqkEVr3WdXqECjQGrjmoZM9c0WZyz4utf";
+		List<GiftUserDeviceVO> resultList = new ArrayList<GiftUserDeviceVO>();
+		
+		String[] registerIdsArr = null; 
 		
 		//FCM 발송 URL
 		String FMCurl = "https://fcm.googleapis.com/fcm/send";
+		
+		String body = null;
+		
+		// 현재 날짜 구하기        
+		Calendar cal = Calendar.getInstance();
+		
+		Calendar getToday = Calendar.getInstance();
+		getToday.setTime(new Date()); //금일 날짜
+		
 
 		try{
-			//알림 발송
-			URL url = new URL(FMCurl);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			resultList = pushList();
+			for(int i = 0 ; i < resultList.size() ; i ++) {
+				//알림 발송
+				URL url = new URL(FMCurl);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				
+				conn.setUseCaches(false);
+				conn.setDoInput(true);
+				conn.setDoOutput(true);
+				conn.setConnectTimeout(5000);
+				conn.setReadTimeout(5000);
+				
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Authorization","key="+authKey);
+				conn.setRequestProperty("Content-Type","application/json");
+				
+				
+				//알림 발송용 Parameter 설정
+				JSONObject json = new JSONObject();
+				JSONArray clientArr = new JSONArray();
+				
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				registerIdsArr = new String[resultList.size()];
 			
-			conn.setUseCaches(false);
-			conn.setDoInput(true);
-			conn.setDoOutput(true);
-			conn.setConnectTimeout(5000);
-			conn.setReadTimeout(5000);
-			
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Authorization","key="+authKey);
-			conn.setRequestProperty("Content-Type","application/json");
-			
-			
-			//알림 발송용 Parameter 설정
-			JSONObject json = new JSONObject();
-			JSONArray clientArr = new JSONArray();
-			
-//			//다건 발송을 위해 Client 목록 작성
-//			StringTokenizer stDevice = new StringTokenizer(deviceId.trim(), ";");
-//            // 넘겨온 핸드폰 번호로 DB에서 Token값 뽑아오는 작업
-//			String deviceArr = "";
-//			while(stDevice.hasMoreElements()){
-//            	// 단말기 Token값 넣어주기
-//				clientArr.add((String)stDevice.nextElement());
-//			}
-			
-			registerIdsArr[0] = "cGcbj2iVd0QtksHx7p2kfl:APA91bF6RdHQImUxkOCunrbAUjdiZIkvKLtiklgIhM6dnRCpcQlJKo_EAWhxaozXcagpaElQXslZj3DjIFL0Bc8CbyyogyoYeE5fwk6eIi6WSmk99QPHbETRMKBFQzpZ7xdlFQKmDe6j";
-			registerIdsArr[1] = "cGcbj2iVd0QtksHx7p2kfl:APA91bF6RdHQImUxkOCunrbAUjdiZIkvKLtiklgIhM6dnRCpcQlJKo_EAWhxaozXcagpaElQXslZj3DjIFL0Bc8CbyyogyoYeE5fwk6eIi6WSmk99QPHbETRMKBFQzpZ7xdlFQKmDe6j";
-			
-
-			//단건의 경우 to, 다건인 경우 registration_ids		
-			json.put("to", "cGcbj2iVd0QtksHx7p2kfl:APA91bF6RdHQImUxkOCunrbAUjdiZIkvKLtiklgIhM6dnRCpcQlJKo_EAWhxaozXcagpaElQXslZj3DjIFL0Bc8CbyyogyoYeE5fwk6eIi6WSmk99QPHbETRMKBFQzpZ7xdlFQKmDe6j");
-//			json.put("registration_ids", registerIdsArr);
-			JSONObject info = new JSONObject();
-			info.put("title", "기프티콘 수첩");
-			info.put("body", "content");
-			info.put("sound", "default");
-			json.put("notification", info);
-			
-			
-			//메시지 발송 처리
-			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-			wr.write(json.toString());
-			wr.flush();
-			
-			
-			//발송 결과
-			OutputStream rostream = conn.getOutputStream();
-		    InputStream ristream = conn.getInputStream();
-
-		    final int length = 50000;
-		    byte[] bytes = new byte[length];
-		    int bytesRead = 0;
-		    
+				registerIdsArr[i] = resultList.get(i).getPush_token();
+				
+				String brand = resultList.get(i).getBrand();
+				String productName = resultList.get(i).getProduct_name();
+				
+				String a_date = format.format(resultList.get(i).getExpiration_period());
+				String b_date = format.format(getToday.getTime());
+				
+				Date a_parseDate = format.parse(a_date);
+		        Date b_parseDate = format.parse(b_date);
 		        
-		    while ((bytesRead = ristream.read(bytes, 0, length)) > 0) {
+		        long resultTime = a_parseDate.getTime() - b_parseDate.getTime();
 		        
-		    	String tmp = new String(bytes, "utf-8");
-
-			//	out.print("{\"multicast_id\":5185012636408180766,\"success\":1,\"failure\":0,\"canonical_ids\":0,\"results\":[{\"message_id\":\"0:1484811869998813%b347b3eab347b3ea\"}]}");
-				if(tmp.trim().length()>0){
-					int beforeLen = tmp.length();
-					int afterLen = 0;
-					rostream.write(bytes, 0, bytesRead);
+//		        System.out.println("초 : "+resultTime / 1000);
+//		        System.out.println("분 : "+resultTime / (60*1000));
+//		        System.out.println("시 : "+resultTime / (60*60*1000));
+//		        System.out.println("일 : "+resultTime / (24*60*60*1000));
+				
+				long dateDiff = resultTime / (24*60*60*1000);
+				
+				if(dateDiff != 0) {
+					body = brand + " '"+productName + "'의 유효기간이 " + dateDiff + "일 남았습니다.";
+				}else {
+					body = brand + " '"+productName + "'의 유효기간이 오늘까지입니다.";
 				}
-		    }	
-
-		    rostream.flush();        
-		        
-		    
-		    if(wr != null){
-		   		wr.close();	
-		    }
-		        
-		    if(ristream != null){
-		    	ristream.close();
-		    }
-		        
-		    if(rostream != null){
-		    	rostream.close();        	
-		    }
 			
+
+				//단건의 경우 to, 다건인 경우 registration_ids		
+				json.put("to", registerIdsArr[i]);
+	//			json.put("registration_ids", registerIdsArr);
+				JSONObject info = new JSONObject();
+				info.put("title", "기프티콘 수첩");
+				info.put("body", body);
+				info.put("sound", "default");
+				json.put("notification", info);
+				
+				
+				//메시지 발송 처리
+				OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+				wr.write(json.toString());
+				wr.flush();
+			
+			
+			
+				//발송 결과
+				OutputStream rostream = conn.getOutputStream();
+			    InputStream ristream = conn.getInputStream();
+	
+			    final int length = 50000;
+			    byte[] bytes = new byte[length];
+			    int bytesRead = 0;
+			    
+			        
+			    while ((bytesRead = ristream.read(bytes, 0, length)) > 0) {
+			        
+			    	String tmp = new String(bytes, "utf-8");
+	
+				//	out.print("{\"multicast_id\":5185012636408180766,\"success\":1,\"failure\":0,\"canonical_ids\":0,\"results\":[{\"message_id\":\"0:1484811869998813%b347b3eab347b3ea\"}]}");
+					if(tmp.trim().length()>0){
+						int beforeLen = tmp.length();
+						int afterLen = 0;
+						rostream.write(bytes, 0, bytesRead);
+					}
+			    }	
+	
+			    rostream.flush();        
+			        
+			    
+			    if(wr != null){
+			   		wr.close();	
+			    }
+			        
+			    if(ristream != null){
+			    	ristream.close();
+			    }
+			        
+			    if(rostream != null){
+			    	rostream.close();        	
+			    }
+			}
 		    return true;
 		}catch(Exception e){
 			e.printStackTrace();
-			logger.info("error ..... \n" + e);
+			logger.info("FCM SEND error \n" + e);
 			return false;
 		}
+	}
+	
+	public List<GiftUserDeviceVO> pushList(){
+
+		Date date = new Date();
+		List<GiftUserDeviceVO> list = new ArrayList<GiftUserDeviceVO>();
+		List<GiftUserDeviceVO> list30 = new ArrayList<GiftUserDeviceVO>();
+		List<GiftUserDeviceVO> list7 = new ArrayList<GiftUserDeviceVO>();
+		List<GiftUserDeviceVO> list1 = new ArrayList<GiftUserDeviceVO>();
+		List<GiftUserDeviceVO> resultList = new ArrayList<GiftUserDeviceVO>();
+		
+		Date[] arr30 = null;
+		Date[] arr7 = null;
+		Date[] arr1 = null;
+		
+	      Calendar cal = Calendar.getInstance();
+	      Calendar cal2 = Calendar.getInstance();
+	      Calendar cal3 = Calendar.getInstance();
+	      
+	      cal.setTime(new Date());
+	      cal2.setTime(new Date());
+	      cal3.setTime(new Date());
+	      DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	
+	      cal.add(Calendar.MONTH, 1);
+	      cal2.add(Calendar.DATE, 7);
+	      cal3.add(Calendar.DATE, 1);
+	      
+	      Date mDate1 = cal.getTime();
+	      Date mDate2 = cal2.getTime();
+	      Date mDate3 = cal3.getTime();
+	      
+		list = giftDAO.getPushList(date);
+		
+		for(int i = 0 ; i < list.size() ; i++) {
+			if(list.get(i).getPush30() == 1) {
+				list30.add(list.get(i));
+			}else if (list.get(i).getPush7() == 1) {
+				list7.add(list.get(i));
+			}else {
+				list1.add(list.get(i));
+			}
+		}
+		
+		
+		//https://www.delftstack.com/ko/howto/java/how-to-compare-two-dates-in-java/#java%25EC%2597%2590%25EC%2584%259C-%25EB%2591%2590-%25EB%2582%25A0%25EC%25A7%259C%25EB%25A5%25BC-%25EB%25B9%2584%25EA%25B5%2590%25ED%2595%2598%25EB%258A%2594before%25EB%25A9%2594%25EC%2586%258C%25EB%2593%259C
+		//date.before(date) 함수 이용해서 list 뽑아내기
+		if(list30.size() != 0) {
+			arr30 = new Date[list30.size()];
+			for(int i = 0 ; i < list30.size(); i++) {
+				arr30[i] = list30.get(i).getExpiration_period();
+				if(arr30[i].equals(mDate1)) {
+					resultList.add(list30.get(i));
+				}else if(arr30[i].before(mDate1)) {
+					resultList.add(list30.get(i));
+				}
+			}
+		}
+		
+		if(list7.size() != 0) {
+			arr7 = new Date[list7.size()];
+			for(int i = 0 ; i < list7.size(); i++) {
+				arr7[i] = list7.get(i).getExpiration_period();
+				if(arr7[i].equals(mDate2)) {
+					resultList.add(list7.get(i));
+				}else if(arr7[i].before(mDate2)) {
+					resultList.add(list7.get(i));
+				}
+			}
+		}
+		
+		if(list1.size() != 0) {
+			arr1 = new Date[list1.size()];
+			for(int i = 0 ; i < list1.size(); i++) {
+				arr1[i] = list1.get(i).getExpiration_period();
+				if(arr1[i].equals(mDate3)) {
+					resultList.add(list1.get(i));
+				}else if(arr1[i].before(mDate3)) {
+					resultList.add(list1.get(i));
+				}
+			}
+		}
+		
+		return resultList;
 	}
 	
 	public LoginVO status(String user_id) {
